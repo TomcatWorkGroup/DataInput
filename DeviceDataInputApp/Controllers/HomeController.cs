2ba2632184e54425fd42520e339546acf179cc1f
Administrator <80201288@qq.com>
2018-03-12 11:21:10 +0800

    增加 GPRS 模块数据接收测试

--- DeviceDataInputApp/Controllers/HomeController.cs
+++ DeviceDataInputApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
 
 namespace DeviceDataInputApp.Controllers
 {
@@ -7,6 +8,24 @@ namespace DeviceDataInputApp.Controllers
         [HttpGet]
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Input()
+        {
+            var stream = Request.Body;
+            byte[] bt = new byte[1024];
+            int length = Request.Body.Read(bt, 0, bt.Length);
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 3; i < length - 2; i += 2)
+            {
+                int x = (bt[i] << 8) + bt[i + 1];
+                sb.Append(x);
+                sb.Append(',');
+            }
+            ViewBag.Str = sb.ToString();
             return View();
         }
     }
