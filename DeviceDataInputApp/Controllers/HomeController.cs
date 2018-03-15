d7af78404cee649bbc1188df037f59a79edff194
80201288 <80201288@qq.com>
2018-03-15 23:19:14 +0800

    HomeController中增加GPRS模块上报数据检测

--- DeviceDataInputApp/Controllers/HomeController.cs
+++ DeviceDataInputApp/Controllers/HomeController.cs
@@ -5,28 +5,42 @@ namespace DeviceDataInputApp.Controllers
 {
     public class HomeController : Controller
     {
+        public static string DATA;
+
         [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Get()
+        {
+            ViewBag.Data = DATA;
+            return View("Input");
+        }
+
+
         [HttpPost]
-        public IActionResult Input()
+        public void Input()
         {
             var stream = Request.Body;
             byte[] bt = new byte[1024];
             int length = Request.Body.Read(bt, 0, bt.Length);
 
             StringBuilder sb = new StringBuilder();
-            for (int i = 3; i < length - 2; i += 2)
+            //for (int i = 3; i < length - 2; i += 2)
+            //{
+            //    int x = (bt[i] << 8) + bt[i + 1];
+            //    sb.Append(x);
+            //    sb.Append(',');
+            //}
+            for (int i = 0; i < length; i++)
             {
-                int x = (bt[i] << 8) + bt[i + 1];
-                sb.Append(x);
-                sb.Append(',');
+                sb.AppendFormat("{0:x2},",bt[i]);
             }
-            ViewBag.Str = sb.ToString();
-            return View();
+
+            DATA = sb.ToString();
         }
     }
 }
